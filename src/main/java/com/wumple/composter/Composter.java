938f4dfb34bb2366aebd90e6b941d877933e94d4
Jason Spangler <jasons@wumple.com>
2018-07-20 19:23:00 -0500

    Jar signing and Curseforge upload deps

--- src/main/java/com/wumple/composter/Composter.java
+++ src/main/java/com/wumple/composter/Composter.java
@@ -1,16 +1,18 @@
 package com.wumple.composter;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.wumple.composter.config.ConfigHandler;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, updateJSON=Reference.UPDATEJSON)
+import com.wumple.composter.config.ConfigHandler;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, updateJSON = Reference.UPDATEJSON, certificateFingerprint=Reference.FINGERPRINT)
 public class Composter
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -35,5 +37,19 @@ public class Composter
     {
         // add any missing default config properties
         ConfigHandler.init();
+    }
+
+    @EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event)
+    {
+        if (logger == null)
+        {
+            logger = LogManager.getLogger(Reference.MOD_ID);
+        }
+        if (logger != null)
+        {
+            logger.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
+            logger.warn("Expected " + event.getExpectedFingerprint() + " found " + event.getFingerprints().toString());
+        }
     }
 }
