d0995f4b7b0fae70292c636c9484ee53d3471fd2
Jason Spangler <jasons@wumple.com>
2018-08-05 14:47:56 -0500

    Library update

--- src/main/java/com/wumple/composter/Composter.java
+++ src/main/java/com/wumple/composter/Composter.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import com.wumple.composter.capability.CompostBinCap;
 import com.wumple.composter.config.ConfigHandler;
+import com.wumple.util.mod.ModBase;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -14,46 +15,46 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, updateJSON = Reference.UPDATEJSON, certificateFingerprint = Reference.FINGERPRINT)
-public class Composter
+public class Composter extends ModBase
 {
     @Mod.Instance(Reference.MOD_ID)
     public static Composter instance;
 
-    public static Logger logger;
-
     @EventHandler
+    @Override
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
-
+        super.preInit(event);
         CompostBinCap.register();
     }
 
     @EventHandler
+    @Override
     public void init(FMLInitializationEvent event)
     {
-        com.wumple.composter.ObjectHolder.RegistrationHandler.registerGuiHandlers();
+        super.init(event);
+        ObjectHolder.RegistrationHandler.registerGuiHandlers();
     }
 
     @EventHandler
+    @Override
     public void postInit(FMLPostInitializationEvent event)
     {
+        super.postInit(event);
         // add any missing default config properties
         ConfigHandler.init();
     }
 
     @EventHandler
+    @Override
     public void onFingerprintViolation(FMLFingerprintViolationEvent event)
     {
-        if (logger == null)
-        {
-            logger = LogManager.getLogger(Reference.MOD_ID);
-        }
-        if (logger != null)
-        {
-            logger.warn("Invalid fingerprint detected! The file " + event.getSource().getName()
-                    + " may have been tampered with. This version will NOT be supported by the author!");
-            logger.warn("Expected " + event.getExpectedFingerprint() + " found " + event.getFingerprints().toString());
-        }
+        super.onFingerprintViolation(event);
+    }
+    
+    @Override
+    public Logger getLoggerFromManager()
+    {
+        return LogManager.getLogger(Reference.MOD_ID);
     }
 }
